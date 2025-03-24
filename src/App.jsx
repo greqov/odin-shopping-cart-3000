265bafd0899d2f2ca692925959acfd65bf8d3852
Ivan Greqov <idgreq@bk.ru>
2025-03-24 17:44:55 +0300

    Replace react-router-dom with react-router, add basic test for a link

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-const App = () => <h1>Such shop</h1>;
+import { Link } from 'react-router';
+
+const App = () => (
+  <>
+    <h1>Such shop</h1>
+    <Link to={{ pathname: '/shop' }}>go shopping</Link>
+  </>
+);
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import { createRoutesStub } from 'react-router';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import { expect } from 'vitest';
 
 describe('App component', () => {
   it('renders correct heading', () => {
-    render(<App />);
+    const Stub = createRoutesStub([
+      {
+        path: '/',
+        Component: App
+      }
+    ]);
+
+    render(<Stub initialEntries={['/']} />);
+
     expect(screen.getByRole('heading').textContent).toMatch(/such shop/i);
+  });
+
+  it('has link to a "Shop" page', () => {
+    const Stub = createRoutesStub([
+      {
+        path: '/',
+        Component: App
+      }
+    ]);
+
+    render(<Stub initialEntries={['/']} />);
+
+    expect(screen.getByText(/go shopping/i)).toBeInTheDocument();
   });
 });
