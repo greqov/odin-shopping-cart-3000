9d8f9624335b926cf04490ae93a15676a6ad6d32
Ivan Greqov <idgreq@bk.ru>
2025-03-25 17:21:43 +0300

    ProductCard: add test for a button handler

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,12 @@
-function ProductCard({ image = '/no-photo.png', title, price = 0 }) {
+function ProductCard({ image = '/no-photo.png', title, price = 0, onClick }) {
   return (
     <div className="product-card">
       <img src={image} alt={title} />
       <h3>{title}</h3>
       <p>${price}</p>
-      <button type="button">Add to cart</button>
+      <button type="button" onClick={onClick}>
+        Add to cart
+      </button>
     </div>
   );
 }
--- src/components/ProductCard.test.jsx
+++ src/components/ProductCard.test.jsx
@@ -1,17 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import ProductCard from './ProductCard';
+import userEvent from '@testing-library/user-event';
 
 describe('ProductCard component', () => {
-  it('has image, title, and price', () => {
+  it('has image, title, price, and button', async () => {
     const image = '/crorodile.png';
     const title = 'Crocodile';
     const price = 8;
+    const onClick = vi.fn(() => 0);
+    const user = userEvent.setup();
 
-    render(<ProductCard title={title} image={image} price={price} />);
+    render(
+      <ProductCard
+        title={title}
+        image={image}
+        price={price}
+        onClick={onClick}
+      />
+    );
 
     expect(screen.getByRole('heading', { name: title })).toBeInTheDocument();
     expect(screen.getByRole('img', { name: title })).toBeInTheDocument();
     expect(screen.getByText(`$${price}`)).toBeInTheDocument();
+
+    const button = screen.getByRole('button', { name: /add to cart/i });
+    expect(button).toBeInTheDocument();
+    await user.click(button);
+    expect(onClick).toHaveBeenCalled();
   });
 
   it('can show default image', () => {
