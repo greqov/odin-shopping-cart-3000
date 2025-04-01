012cffd51598bae32029e47b7c7503eb8c0154dd
Ivan Greqov <idgreq@bk.ru>
2025-04-01 22:26:32 +0300

    ShoppingCart: add test for products in a cart

--- src/pages/ShoppingCart.test.jsx
+++ src/pages/ShoppingCart.test.jsx
@@ -1,9 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import {
-  createRoutesStub,
-  RouterProvider,
-  createMemoryRouter
-} from 'react-router';
+import { RouterProvider, createMemoryRouter, Outlet } from 'react-router';
 import routes from '../routes';
 import ShoppingCart from './ShoppingCart';
 
@@ -28,5 +24,42 @@ describe('ShoppingCart component', () => {
     render(<RouterProvider router={router} />);
 
     expect(screen.getByText(/cart is empty/i)).toBeInTheDocument();
+  });
+
+  it('shows product item if there is a product in a cart', () => {
+    const cart = [
+      {
+        product: {
+          id: 2,
+          title: 'Alpaca',
+          image: '/alpaca.png',
+          price: 3
+        },
+        quantity: 2
+      }
+    ];
+    const setCart = vi.fn(() => 0);
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: '/',
+          element: <Outlet context={[cart, setCart]}></Outlet>,
+          children: [
+            {
+              path: 'shopping-cart',
+              element: <ShoppingCart />
+            }
+          ]
+        }
+      ],
+      {
+        initialEntries: ['/shopping-cart']
+      }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByText(/Alpaca, 2/i)).toBeInTheDocument();
   });
 });
